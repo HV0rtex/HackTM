88a681a9377b3fb6c07a44a3ff546a9e58e9916a
David Bogdan <david.bnicolae@gmail.com>
2023-05-13 23:13:02 +0300

    [BACKEND] Added AI action URL

--- td/views.py
+++ td/views.py
@@ -2,6 +2,25 @@ from django.template.loader import render_to_string
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import json
+import numpy as np
+
+from td.sources.ai2 import Agent, perform_action
+
+agent = Agent()
+prev = np.zeros(shape=(10000,))
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def request_ai_action(request, reward):
+    if request.method == "POST":
+        state = request.body.decode('utf-8')
+        state = json.loads(state)
+
+        act = agent.act(state)
+        agent.learn(prev, act, reward, state)
+        prev = state
+
+        return HttpResponse(perform_action(act))
